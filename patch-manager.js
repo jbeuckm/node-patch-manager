edf72f2acf044c62c44a27e9baa5180c0fac908b
joe beuckman <joebeuckman@gmail.com>
2015-04-28 22:03:03 -0500

    collecting interface and midi channel from command line

--- patch-manager.js
+++ patch-manager.js
@@ -1,5 +1,9 @@
+#!/usr/bin/env node
+
+var Q = require('q');
+var program = require('commander');
+var prompt = require('prompt');
 var fs = require('fs');
-var csv = require('fast-csv');
 var midi = require('midi');
 var Parser = require('midi-parser');
 var msg = Parser.msg;
@@ -7,80 +11,144 @@ var msg = Parser.msg;
 var input = new midi.input();
 var parser = new Parser();
 
-console.log("Available MIDI ins:");
-var portCount = input.getPortCount();
-for (var i=0; i<portCount; i++) {
-    console.log(i+": "+input.getPortName(i));
-}
 
-
-input.on('message', function(deltaTime, message) {
+input.on('message', function (deltaTime, message) {
     parser.write(message);
 });
 
-parser.on('midi', function(status, channel, message) {
+parser.on('midi', function (status, channel, message) {
 
     switch (status) {
 
-    case msg.ctrlChg:
+        case msg.ctrlChg:
 //	console.log("Control Change ("+channel+") "+message);
-	handleControllerMessage(message);
-	break;
+            handleControllerMessage(message);
+            break;
 
-    case msg.progChg:
+        case msg.progChg:
 //	console.log("Program Change ("+channel+") "+message);
-	handleProgramChange(message);
-	break;
+            handleProgramChange(message);
+            break;
 
     }
 });
 
-input.openPort(1);
-
-// Sysex, timing, and active sensing
-input.ignoreTypes(false, true, false);
 
 // Write Sound Sysex Command: F0 41 6n 21 F7
 
 
-//input.closePort();
-
-var stream = fs.createReadStream("data/programs.csv");
-
-var programs = [];
-var currentProgramNumber = 0;
+var patches = [];
 var currentBankNumber = 0;
+var currentProgramNumber = 0;
 
-csv
-    .fromStream(stream, {
-        headers: true
-    })
-    .on("data", function(program){
-	programs.push(program);
-    })
-    .on("end", function () {
-	console.log("loaded programs");
-	console.log(programs);
-
-	programs.push({ 'name': 'new' });
-
-	savePrograms();
-    });
 
 function handleControllerMessage(message) {
-    console.log(message);
+    console.log("handleControllerMessage with " + message);
+    console.log(typeof message);
 }
 function handleProgramChange(message) {
-    console.log(message);
+    console.log("handleProgramChange with " + message);
+    console.log(typeof message);
 }
 
-function savePrograms() {
 
-    csv
-	.writeToPath("data/bank"+currentBankNumber+".csv", 
-		     programs, {headers: true})
-	.on("finish", function(){
-	    console.log("done!");
-	});
+function loadPatches() {
+
+    fs.readdir(__dirname + '/data', function (err, files) {
+        files.filter(function (file) {
+            return file.substr(-5) === '.json';
+        })
+            .forEach(function (file) {
+
+                loadPatch(file);
+            });
+    });
+
 }
+
+function loadPatch(filename) {
+
+    fs.readFile(filename, 'utf8', function (err, data) {
+        if (err) {
+            console.log('Error: ' + err);
+            return;
+        }
+
+        data = JSON.parse(data);
+
+        patches = data;
+        console.dir(patches);
+
+    });
+
+}
+
+function saveCurrentPatch() {
+
+    var data = JSON.stringify(patches);
+
+    var filename = __dirname + '/bank' + currentBankNumber + '/' + currentProgramNumber + '.json';
+
+    fs.writeFile(filename, data, function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+
+program
+    .version('0.0.1')
+    .option('-i, --interface [number]', 'Select MIDI interface')
+    .option('-c, --channel [number]', 'Respond to MIDI channel [1]', 1)
+    .parse(process.argv);
+
+
+var interfaceDef = Q.defer();
+
+console.log('interface = ' + program.interface);
+
+if (program.interface) {
+    interfaceDef.resolve(program.interface);
+}
+else {
+
+    console.log("Available MIDI ins:");
+    var portCount = input.getPortCount();
+    for (var i = 0; i < portCount; i++) {
+        console.log(i + ": " + input.getPortName(i));
+    }
+
+    var schema = {
+        properties: {
+            interface: {
+                pattern: /^[0-9]+$/,
+                message: 'Interface must be an integer',
+                required: true
+            }
+        }
+    };
+
+    prompt.start();
+
+    prompt.get(schema, function (err, result) {
+        if (err) {
+            console.log(err);
+            interfaceDef.reject(err);
+        }
+        else {
+            interfaceDef.resolve(result);
+        }
+    });
+}
+
+interfaceDef.promise.then(function(interface){
+
+    input.openPort(parseInt(interface));
+// Sysex, timing, and active sensing
+    input.ignoreTypes(false, true, false);
+
+});
+
+loadPatches();
 
